817aec4b977b1b77a98b740bdeef355471f6b204
Costin Grigoras <costin.grigoras@cern.ch>
2018-02-08 11:07:40 +0100

    Drop local testing scripts

--- updateAFS.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp /home/costing/workspace/MSRC/MonaLisa/Service/lib/{FarmControl,FarmMonitor,JFarmMonitor}.jar aliprod@lxplus:/afs/cern.ch/alice/alien2/.i386_linux24/alien/java/MonaLisa/Service/lib
-scp /home/costing/workspace/MSRC/MonaLisa/Service/lib/{FarmControl,FarmMonitor,JFarmMonitor}.jar aliprod@lxplus:/afs/cern.ch/alice/alien2/.amd64_linux24/alien/java/MonaLisa/Service/lib
-scp /home/costing/workspace/MSRC/MonaLisa/Service/lib/{FarmControl,FarmMonitor,JFarmMonitor}.jar root@pcalienstorage:/opt/alien64/alien/java/MonaLisa/Service/lib/
--- updateShared.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#scp /home/costing/workspace/MSRC/MonaLisa/Service/lib/{FarmControl,FarmMonitor,JFarmMonitor}.jar root@pcalienstorage:/opt/alien32/alien//java/MonaLisa/Service/lib/
-scp /home/costing/workspace/MSRC/MonaLisa/Service/lib/*.jar /home/costing/workspace/lazyj/build/lazyj.jar root@pcalienstorage:/opt/alien64/alien/java/MonaLisa/Service/lib/
-#scp /home/costing/workspace/MSRC/MonaLisa/Service/lib/*.jar /home/costing/workspace/MSRC/WEBS/WEB_GRAPH/WEB-INF/lib/lazyj.jar root@pcalienstorage:/opt/alien64/alien.NEW/java/MonaLisa/Service/lib/
-#scp /home/costing/workspace/MSRC/MonaLisa/Service/lib/*.jar /home/costing/workspace/MSRC/WEBS/WEB_GRAPH/WEB-INF/lib/lazyj.jar root@pcalienstorage:/opt/alien64/alien.FINAL/java/MonaLisa/Service/lib/
-#scp /home/costing/workspace/MSRC/MonaLisa/Service/lib/*.jar /home/costing/workspace/MSRC/WEBS/WEB_GRAPH/WEB-INF/lib/lazyj.jar root@pcalienstorage2:/opt/alien64/alien/java/MonaLisa/Service/lib/
