983cbcba7151df32e60b1b56ad5f4f0a5a8e092a
Costin Grigoras <costin.grigoras@cern.ch>
2018-02-08 10:39:58 +0100

    Allocate array lengths of the correct size from the beginning, to avoid reallocations later on

--- src/lia/Monitor/monitor/GenericUDPResult.java
+++ src/lia/Monitor/monitor/GenericUDPResult.java
@@ -8,52 +8,45 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * 
+ *
  * @author ramiro
  */
 public class GenericUDPResult {
 
-    public long rtime;
-    public long senderID;
-    public String clusterName;
-    public String nodeName;
-    public final List paramValues;
-    //this was added to keep track of params order ...
-    public final List paramNames;
+	public long rtime = 0;
+	public final long senderID;
+	public String clusterName;
+	public String nodeName;
+	public final List<Object> paramValues;
+	// this was added to keep track of params order ...
+	public final List<String> paramNames;
 
-    public GenericUDPResult() {
-        senderID = -1;
-        paramValues = new ArrayList();
-        paramNames = new ArrayList();
-    }
+	public GenericUDPResult(final int initialSize, final Long srcID) {
+		senderID = srcID != null ? srcID.longValue() : -1;
+		paramValues = new ArrayList<Object>(initialSize);
+		paramNames = new ArrayList<String>(initialSize);
+	}
 
-    public void addParam(String name, Object value) {
-        if (name == null || value == null) {
-            return;
-        }
-        if (name.trim().length() == 0) {
-            return;
-        }
-        paramNames.add(name);
-        paramValues.add(value);
-    }
+	public void addParam(final String name, final Object value) {
+		paramNames.add(name);
+		paramValues.add(value);
+	}
 
-    //Just for Debugging 
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(" [ ").append(new Date(rtime)).append(" ] ");
-        sb.append("Cluster:").append(clusterName).append("\t");
-        sb.append("Node:").append(nodeName).append("\t");
+	// Just for Debugging
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append(" [ ").append(new Date(rtime)).append(" ] ");
+		sb.append("Cluster:").append(clusterName).append("\t");
+		sb.append("Node:").append(nodeName).append("\t");
 
-        if (paramValues != null && paramValues.size() > 0) {
-            final int len = paramValues.size();
-            for (int i = 0; i < len; i++) {
-                sb.append(paramNames.get(i)).append(" = ").append(paramValues.get(i)).append("\t");
-            }
-        } else {
-            sb.append(" No PARAMS!");
-        }
-        return sb.toString();
-    }
+		if (paramValues != null && paramValues.size() > 0) {
+			final int len = paramValues.size();
+			for (int i = 0; i < len; i++)
+				sb.append(paramNames.get(i)).append(" = ").append(paramValues.get(i)).append("\t");
+		}
+		else
+			sb.append(" No PARAMS!");
+		return sb.toString();
+	}
 }
-
